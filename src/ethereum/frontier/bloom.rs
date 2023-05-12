69edbec241b9d00a57ab3350ad016398187c3c26
andy-thomason <andy@atomicinrement.com>
2023-05-12 18:24:59 +0100

    Fix bad chars

--- src/ethereum/frontier/bloom.rs
+++ src/ethereum/frontier/bloom.rs
@@ -6,8 +6,8 @@ use super::fork_types::{keccak256, Log, Bloom};
 /// ^^^^^^^^^^^^^^^^^^^
 /// 
 /// .. contents:: Table of Contents
-///     :backlinks: none
-///     :local:
+/// :backlinks: none
+/// :local:
 /// 
 /// Introduction
 /// ------------
@@ -19,18 +19,18 @@ use super::fork_types::{keccak256, Log, Bloom};
 /// eliminating blocks and reciepts from their search.
 /// 
 /// 
-///     Add a bloom entry to the bloom filter (`bloom`).
+/// Add a bloom entry to the bloom filter (`bloom`).
 /// 
-///     The number of hash functions used is 3. They are calculated by taking the
-///     least significant 11 bits from the first 3 16-bit words of the
-///     `keccak_256()` hash of `bloom_entry`.
+/// The number of hash functions used is 3. They are calculated by taking the
+/// least significant 11 bits from the first 3 16-bit words of the
+/// `keccak_256()` hash of `bloom_entry`.
 /// 
-///     Parameters
-///     ----------
-///     bloom :
-///         The bloom filter.
-///     bloom_entry :
-///         An entry which is to be added to bloom filter.
+/// Parameters
+/// ----------
+/// bloom :
+///     The bloom filter.
+/// bloom_entry :
+///     An entry which is to be added to bloom filter.
 /// 
 ///
 pub fn add_to_bloom(bloom: &mut Bloom, bloom_entry: &[u8]) {
@@ -46,21 +46,21 @@ pub fn add_to_bloom(bloom: &mut Bloom, bloom_entry: &[u8]) {
 
 
 /// 
-///     Obtain the logs bloom from a list of log entries.
+/// Obtain the logs bloom from a list of log entries.
 /// 
-///     The address and each topic of a log are added to the bloom filter.
+/// The address and each topic of a log are added to the bloom filter.
 /// 
-///     Parameters
-///     ----------
-///     logs :
-///         List of logs for which the logs bloom is to be obtained.
+/// Parameters
+/// ----------
+/// logs :
+///     List of logs for which the logs bloom is to be obtained.
 /// 
-///     Returns
-///     -------
-///     logs_bloom : `Bloom`
-///         The logs bloom obtained which is 256 bytes with some bits set as per
-///         the caller address and the log topics.
-///     
+/// Returns
+/// -------
+/// logs_bloom : `Bloom`
+///     The logs bloom obtained which is 256 bytes with some bits set as per
+///     the caller address and the log topics.
+/// 
 pub fn logs_bloom(logs: &[Log]) -> Bloom {
     let mut bloom = [0; 256];
     for log in logs {
